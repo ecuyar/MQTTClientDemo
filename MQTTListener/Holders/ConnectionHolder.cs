9f26fa773f6c71049416a7eeae553bf9d50a3f99
Enes Can UYAR <uyar.enescan@gmail.com>
2023-04-12 00:46:54 +0300

    seperate connection and pages-1

---
+++ MQTTListener/Holders/ConnectionHolder.cs
@@ -0,0 +1,49 @@
+﻿using MQTTnet.Client;
+using MQTTnet;
+using MQTTListener.Dtos;
+using MQTTnet.Server;
+
+namespace MQTTListener.Holders
+{
+	public class ConnectionHolder
+	{
+		public IMqttClient MqttClient { get; set; }
+		private readonly MqttFactory MqttFactory;
+		private ConnectionProps ConnectionProps { get; set; }
+
+		public ConnectionHolder()
+		{
+			MqttFactory = new MqttFactory();
+			MqttClient = MqttFactory.CreateMqttClient();
+			ConnectionProps = new ConnectionProps()
+			{
+				ServerIp = "wss://a989a75157744b01a88b4876006e827d.s2.eu.hivemq.cloud:8884/mqtt",
+				Port = 8884,
+				Username = "mqttdeneme",
+				Password = "mqttdeneme",
+				ClientId = Guid.NewGuid()
+			};
+		}
+
+		public async Task ConnectToServer()
+		{
+			var mqttOptions = new MqttClientOptionsBuilder()
+				.WithWebSocketServer(ConnectionProps.ServerIp)
+				.WithCredentials(ConnectionProps.Username, ConnectionProps.Password)
+				.WithClientId(ConnectionProps.ClientId.ToString())
+				.WithTls()
+				.Build();
+
+			MqttClientConnectResult response = new();
+			try
+			{
+				response = await MqttClient.ConnectAsync(mqttOptions);
+			}
+			catch (Exception ex)
+			{
+				//await IsConnectedChanged.InvokeAsync(false);
+				//ErrorMessage = ex.Message;
+			}
+		}
+	}
+}
---
+++ MQTTListener/Holders/MessageHolder.cs
@@ -0,0 +1,57 @@
+﻿namespace MQTTListener.Holders
+{
+	public class MessageHolder
+	{
+		public int MessageCapacity { get; init; }
+		public List<Message> MessageList { get; set; } = new();
+
+		public MessageHolder(int MessageCount)
+		{
+			MessageCapacity = MessageCount;
+			MessageList = new();
+		}
+
+		public void AddMessage(string message)
+		{
+			int count = MessageList.Count;
+
+			if (count >= MessageCapacity)
+			{
+				RemoveLastMessage();
+			}
+
+			var m = new Message(count, message);
+			MessageList.Add(m);
+		}
+
+		public List<Message> ShowMessages()
+		{
+			return MessageList.OrderByDescending(x => x.IndexValue).ToList();
+		}
+
+		private void RemoveLastMessage()
+		{
+			if (MessageList.Count >= MessageCapacity)
+			{
+				MessageList.RemoveAt(MessageList.Count - 1);
+
+				foreach (var listItem in MessageList)
+				{
+					listItem.IndexValue -= 1;
+				}
+			}
+		}
+
+		public class Message
+		{
+			public int IndexValue { get; set; }
+			public string MessageString { get; set; }
+
+			public Message(int index, string message)
+			{
+				IndexValue = index;
+				MessageString = message;
+			}
+		}
+	}
+}
